07248845af88a5dc0f80de0aab96eca64855d181
Hubert Roszyk <roszyk.hubert@gmail.com>
2024-10-13 20:01:10 +0200

    Added counting and sorting logic

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 document_list = []
 document_count = int(input("How many documents you want to add?: "))
 for i in range(0, document_count):
-    document_list.append(input("Input document: "))
+    document_list.append(input("Input document: ").strip())
 
 search_list = []
 search_count = int(input("How many words you want to search?: "))
 for i in range(0, search_count):
     while True:
-        word = input("Input the word: ")
+        word = input("Input the word: ").strip()
         if word in search_list:
             print("This word already exists")
         else:
             search_list.append(word)
             break
 
-print(document_list)
-print(search_list)
+def count_word_in_document(word, document):
+    return document.lower().split().count(word.lower())
+
+for word in search_list:
+    document_word_count = []
+    for idx, doc in enumerate(document_list):
+        word_count = count_word_in_document(word, doc)
+        if word_count > 0:
+            document_word_count.append((idx, word_count))
+    
+    sorted_documents = sorted(document_word_count, key=lambda x: x[1], reverse=True)
+    
+    sorted_document_indices = [doc[0] for doc in sorted_documents]
+    
+
+    print(sorted_document_indices)
